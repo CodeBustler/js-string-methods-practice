31ceebd070389b3688d2e5fba1906fa7315ba28d
CodeBustler <codebustler360online@gmail.com>
2023-06-12 19:30:14 +0530

    1 file modified

--- script.js
+++ script.js
@@ -2,60 +2,60 @@ let log = console.log;
 // String Methods
 
 // 01. length Property
-let string = 'CodeBustler Coder ';
-log(string.length); // 11
+let string = 'CodeBustler';
+// log(string.length); // 11
 
 // 02. includes(searchValue, startIndex) | Returns Boolean
-log(string.includes('B', 4)); // true
+// log(string.includes('B', 4)); // true
 
 // 03. toUpperCase()
-log(string.toUpperCase()); // CODEBUSTLER
+// log(string.toUpperCase()); // CODEBUSTLER
 
 // 04. toLowerCase()
-log(string.toLowerCase()); // codebustler
+// log(string.toLowerCase()); // codebustler
 
 // 05. startsWith(searchValue, start) | Returns Boolean
-log(string.startsWith('Bus', 4)); // True
+// log(string.startsWith('Bus', 4)); // True
 
 // 06. endsWith(searchValue, start) | Returns Boolean
-log(string.startsWith('Bus', 4));
+// log(string.endsWith('er', 11)); // true
 
-// 07. search(searchValue) | Returns first match (-1 if no match)
-// ** If the search value is a stringing, it is converted to a regular expression.
+// 07. search(searchValue) | Returns first match index (-1 if no match)
+// ** If the search value is a string, it is converted to a regular expression.
 log(string.search('de')); // 2
 
 // 08. match(match) | match based on regExp*
 // Returns array of info, if no match returns null*
-log(string.match(/Code/g)); // ["code", "Code"]
+// log(string.match(/Code/g)); // ["code", "Code"]
 
 // 09. indexOf(searchvalue, start) | Retuns index (first occurence)
-log(string.indexOf('ode')); //1
+// log(string.indexOf('ode')); //1
 
 // 10. lastIndexOf() | Retuns index (*last occurence)
-log(string.lastIndexOf('ode')); //13
+// log(string.lastIndexOf('ode')); //13
 
 // 11. trim() | Removes whitespace from both ends of a string.
 let strSpace = ' code bustler ';
 let trimString = strSpace.trim();
-log(strSpace.length); // 14 (With Space)
-log(trimString.length); // 12 (Without Space)
+// log(strSpace.length); // 14 (With Space)
+// log(trimString.length); // 12 (Without Space)
 
 // 12. trimStart() | Removes whitespace from start of a string.
 let trimStart = strSpace.trimStart();
-log(strSpace.length); // 14 (With Start Space)
-log(trimStart.length); // 13 (Without Start Space)
+// log(strSpace.length); // 14 (With Start Space)
+// log(trimStart.length); // 13 (Without Start Space)
 
 // 13. trimEnd() | Removes whitespace from start of a string.
 let trimEnd = strSpace.trimEnd();
-log(strSpace.length); // 14 (With End Space)
-log(trimStart.length); // 13 (Without End Space)
+// log(strSpace.length); // 14 (With End Space)
+// log(trimStart.length); // 13 (Without End Space)
 
 // 14. at(index) | Returns indexed single character | ES12(2021)
-log(string.at(4) + ' | By using at()'); // B
-log(string[2]); // d | this also possible for string char
+// log(string.at(4) + ' | By using at()'); // B
+// log(string[2]); // d | this also possible for string char
 
 // 15. charAt(index) | Returns
-log(string.charAt(4) + ' | By using chatAt()'); // B
+// log(string.charAt(4) + ' | By using chatAt()'); // B
 
 // DIFFERENCE at() & charAt
 // The at(index) method properly handles non-BMP Unicode characters and returns them as a single character, (BMP : Basic Multilingual Plane)
@@ -63,23 +63,23 @@ log(string.charAt(4) + ' | By using chatAt()'); // B
 // The charAt(index) method treats non-BMP Unicode characters as a surrogate pair and returns the first half of the surrogate pair (which is not a valid character on its own).
 
 // 16. charCodeAt(index) | Returns Unicode of the character at a specified index (position)
-log(string.charCodeAt('4')); // 66 (unicode of B)
+// log(string.charCodeAt('4')); // 66 (unicode of B)
 
 //-----------------
 
 // 17. String.fromCharCode(n1, n2, ..., nX) | Return string created by using specified sequance of unicode values
-log(String.fromCharCode(66, 66, 67)); // BBC
+// log(String.fromCharCode(66, 66, 67)); // BBC
 
 // 18. concat(string1, string2, ..., stringX) | joins two or more strings.
 let stng1 = 'CodeBustler';
 let stng2 = ' All About Coding';
 let newString = stng1.concat(stng2);
-log(newString);
+// log(newString);
 
 // 19. replace(searchValue, newValue)
 let cities = 'Hyderabad, Bangalore, Mysore';
 let updatedCities = cities.replace('Hyderabad', 'Delhi');
-log(updatedCities); // Delhi, Bangalore, Mysore
+// log(updatedCities); // Delhi, Bangalore, Mysore
 
 // You can use regular expression replace all matching string in replace()
 // >>>  input = input.replace(/test/g, 'java');
@@ -97,7 +97,7 @@ let replaceString = 'bikes';
 
 let myNewString = myString.replaceAll(searchString, replaceString);
 
-console.log(myNewString); // I love bikes, and bikes are costly
+// console.log(myNewString); // I love bikes, and bikes are costly
 
 // REGULAR EXPRESSION
 // String inputString="es2021 first sample code first";
@@ -109,12 +109,12 @@ console.log(myNewString); // I love bikes, and bikes are costly
 // 21.  split(separator, limit) | splits a string into an array of substrings
 let my_string = 'Hi, Good evening this is codebustler!';
 let myArray = my_string.split('');
-log(myArray);
+// log(myArray);
 // document.getElementsByTagName('h2')[0].innerHTML = myArray;
 
 // 22. repeat(count) | Returns a new string with a number of copies of a string
 let strNew = string.repeat(5);
-log(strNew);
+// log(strNew);
 
 // 23. slice(start, end) | Extracts a part of a string in a new string
 let strr = 'CodeBustler Coder';
@@ -123,16 +123,16 @@ let sliceString = strr.slice(0, 4); // Code
 let sliceString1 = strr.slice(0); // CodeBustler Code (Whole String)
 let sliceString2 = strr.slice(-1); // r (last String)
 
-log(sliceString);
-log(sliceString1);
-log(sliceString2);
+// log(sliceString);
+// log(sliceString1);
+// log(sliceString2);
 
 // 24. substring(start, *end) | Extracts characters from start to end (positions)
-log(strr.substring(0, 11)); //CodeBustler
-log(strr.substring(12, 17)); //Coder
+// log(strr.substring(0, 11)); //CodeBustler
+// log(strr.substring(12, 17)); //Coder
 
 // 25. substr(start, *length) | Extracts a part of a string
-log(strr.substr(4, 3)); // Bustler
+// log(strr.substr(4, 3)); // Bustler
 
 //------------------
 
@@ -142,20 +142,20 @@ log(strr.substr(4, 3)); // Bustler
 let numb = 5;
 let text3 = numb.toString();
 let padded = text3.padStart(3, '*');
-log(padded); // **5
+// log(padded); // **5
 
 // 27. padEnd(length, string) | Pads a string at the end.
 let padded2 = text3.padEnd(3, '*');
-log(padded2); // 5**
+// log(padded2); // 5**
 
 //------------------
 
-// toString() | Returns a string as a string | can be used to convert a string object into a string. ** Every JavaScript object has a toString() method. Normally, you **will not use it in your own code.
+// 28. toString() | Returns a string as a string | can be used to convert a string object into a string. ** Every JavaScript object has a toStr ing() method. Normally, you **will not use it in your own code.
 let text = 'Hello World!';
 let result = text.toString();
 // log(result); // Hello World!
 
-// valueOf()
+// 29. valueOf()
 // This method is the default method for JavaScript strings.
 // It is used internally by JavaScript.
 // Normally, you will not use it in your code.
